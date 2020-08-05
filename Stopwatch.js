b9120447a7d8517dc8f851fcf39fb7be478e1933
Sergio Cazaresmuniz <cazaresmuniz.sergio@gmail.com>
2020-08-05 11:18:13 -0500

    Created a function for Stopwatch

--- Stopwatch.js
+++ Stopwatch.js
@@ -2,8 +2,8 @@ window.onload = function() {
 //created a variables for my stopwatch
     var seconds = 00;
     var tens = 00;
-    var appendTens = document.getElementById("tens")
-    var appendSeconds = document.getElementById("seconds")
+    var appendTens = document.getElementById("tens");
+    var appendSeconds = document.getElementById("seconds");
     var buttonStart = document.getElementById("button-start");
     var buttonStop = document.getElementById("button-stop");
     var buttonReset = document.getElementById('button-reset');
@@ -26,4 +26,28 @@ window.onload = function() {
         appendTens.innerHTML = tens;
         appendSeconds.innerHTML = seconds;
     }
+//Created function for the stopwatch to work
+ function startTimer () {
+        tens++;
+
+        if(tens < 9){
+            appendTens.innerHTML = "0" + tens;
+        }
+
+        if (tens > 9){
+            appendTens.innerHTML = tens;
+        }
+
+        if (tens > 99){
+            console.log("seconds");
+            seconds++;
+            appendSeconds.innerHTML = "0" + seconds;
+            tens = 0;
+            appendTens.innerHTML = "0" + 0;
+        }
+
+        if (seconds > 9){
+            appendSeconds.innerHTML = seconds;
+        }
+ }
 }
