77e1d7b39e55ad08f6c0199b208babccbbf309c9
Sergio Cazaresmuniz <cazaresmuniz.sergio@gmail.com>
2020-08-05 11:09:05 -0500

    created onclick function

--- Stopwatch.js
+++ Stopwatch.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-
+//created a variables for my stopwatch
     var seconds = 00;
     var tens = 00;
     var appendTens = document.getElementById("tens")
@@ -7,4 +7,23 @@ window.onload = function() {
     var buttonStart = document.getElementById("button-start");
     var buttonStop = document.getElementById("button-stop");
     var buttonReset = document.getElementById('button-reset');
+    var Interval ;
+//created .onclick which xecute a JavaScript when a button is clicked
+    buttonStart.onclick = function() {
+
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 10);
+    }
+
+    buttonStop.onclick = function() {
+        clearInterval(Interval);
+    }
+
+    buttonReset.onclick = function() {
+        clearInterval(Interval);
+        tens = "00";
+        seconds = "00";
+        appendTens.innerHTML = tens;
+        appendSeconds.innerHTML = seconds;
+    }
 }
